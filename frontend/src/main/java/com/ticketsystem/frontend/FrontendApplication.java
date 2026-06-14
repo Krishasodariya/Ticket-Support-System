99715279fc3c06337ff4cb38295ce9812c263606
Krishhimself <168916217+Krishasodariya@users.noreply.github.com>
2026-06-14 17:49:36 +0200

    Add files via upload

--- frontend/src/main/java/com/ticketsystem/frontend/FrontendApplication.java
+++ frontend/src/main/java/com/ticketsystem/frontend/FrontendApplication.java
@@ -1,6 +1,7 @@
 package com.ticketsystem.frontend;
 
 import com.ticketsystem.frontend.util.Navigator;
+import com.ticketsystem.frontend.util.RealtimeWebSocketClient;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -11,6 +12,11 @@ public class FrontendApplication extends Application {
        primaryStage.setTitle("Ticket Support System");
         Navigator.setPrimaryStage(primaryStage);
         Navigator.navigateToHome();
+    }
+
+    @Override
+    public void stop() {
+        RealtimeWebSocketClient.getInstance().disconnect();
     }
 
     public static void main(String[] args) {
